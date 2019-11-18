ab528035132915f16a7775866260316b86475737
Abhilash C <30768127+abhilashc708@users.noreply.github.com>
2019-11-18 20:42:26 +0530

    Springboot-paytm-payment-gateway
    Spring boot API with paytm payment gate way integration

---
+++ gateway/src/main/java/com/payment/gateway/app/PaymentGatewayApp.java
@@ -0,0 +1,13 @@
+package com.payment.gateway.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PaymentGatewayApp {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayApp.class, args);
+	}
+
+}
